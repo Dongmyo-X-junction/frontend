9ae04ca662e1715cbe2cb927d6ab646ca5d7d700
90bytes <msseo@90byt.es>
2021-11-20 00:51:55 +0900

    Revert "feat: add dashboard component"
    This reverts commit 25c00760d1fbff94028542e5208f85e0c1ba41d5.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import 'antd/dist/antd.css';
 import './App.css';
-import {Route, Switch} from "react-router-dom";
-import Dashboard from "./page/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Dashboard/>
-        </Route>
-      </Switch>
     </div>
   );
 }
